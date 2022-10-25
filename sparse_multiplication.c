fe1e6695fb97eba7a206e7e09b4a967daa1e4587
ApolloStaff <114802700+ApolloStaff@users.noreply.github.com>
2022-10-25 15:37:55 +0530

    Code refinement
    While testing the code, I found that ```c[i+j].coeff``` was getting reassigned different values as i and j were incrementing in the nested for loop of ```c[i + j].coeff += a[i].coeff * b[j].coeff;```, that is c[0+1] and c[1+0] was assigning the same coefficient to two completely different variables of the polynomial. A few changes have been made to the code to address this.  
    
    - Instead of storing the coefficients and exponents in the ```c[i+j]``` index, I stored it in ```c[k]```.
    - This gave me the correct results, however the variables were no longer grouped. Another loop was introduced in the code that targeted identical coefficients of the resultant polynomial and its corresponding coefficients were added.

--- sparse_multiplication.c
+++ sparse_multiplication.c
@@ -17,7 +17,7 @@ int main()
 		printf("Enter the coefficient(%d): ", i + 1);
 		scanf("%d", &a[i].coeff);
 		printf("Enter the exponent(%d): ", i + 1);
-		scanf("%d", &a[i].expo);        /* only statement in loop */
+		scanf("%d", &a[i].expo);       	
 	}
 	printf("\nEnter the total number of terms in the polynomial two:");
 	scanf("%d", &t2);
@@ -27,23 +27,23 @@ int main()
 		printf("Enter the coefficient(%d): ", i + 1);
 		scanf("%d", &b[i].coeff);
 		printf("Enter the exponent(%d): ", i + 1);
-		scanf("%d", &b[i].expo);       
+		scanf("%d", &b[i].expo);
 	}
 	printf(" \nThe first polynomial is: ");
-	printf("%d(x^%d)",a[0].coeff,a[0].expo);
-	for (int k = 1; k < t1; k++) 
+	printf("%d(x^%d)", a[0].coeff, a[0].expo);
+	for (int k = 1; k < t1; k++)
 	{
 		printf("+%d(x^%d)", a[k].coeff, a[k].expo);
 	}
 	printf(" \nThe second polynomial is: ");
-	printf("%d(x^%d)",b[0].coeff,b[0].expo);
-	for (int k = 1; k < t2; k++) 
+	printf("%d(x^%d)", b[0].coeff, b[0].expo);
+	for (int k = 1; k < t2; k++)
 	{
 		printf("+%d(x^%d)", b[k].coeff, b[k].expo);
 	}
-	int t3=t1+t2;
+	int t3 = t1 * t2;
 	struct poly c[100];
-	for (int i = 0; i < t3; i++) 
+	for (int i = 0; i < t3; i++)
 	{
 		c[i].coeff = 0;
 		c[i].expo = 0;
@@ -56,20 +56,38 @@ int main()
 	j = 0;
 	k = 0;
 
-	for (i = 0; i < t1; i++) {
-		for (j = 0; j < t2; j++) {
-			c[i + j].coeff += a[i].coeff * b[j].coeff;
-			c[i + j].expo = a[i].expo + b[j].expo;
+	for (i = 0; i < t1; i++)
+	{
+		for (j = 0; j < t2; j++) 
+		{
+			c[k].coeff += a[i].coeff * b[j].coeff;
+			c[k].expo = a[i].expo + b[j].expo;
 			k++;
 		}
 
 	}
 	printf(" \nResultant polynomial: ");
-	printf("%d(x^%d)",c[0].coeff,c[0].expo);
-	for (int k = 1; k < t3; k++) 
+	printf("%d(x^%d)", c[0].coeff, c[0].expo);
+	for (int k = 1; k < t3; k++)
 	{
 		printf("+%d(x^%d)", c[k].coeff, c[k].expo);
 	}
-
+	for (i = 0; c[i].expo != 0; i++)
+	{
+		for (j = i + 1; c[j].expo != 0; j++)
+		{
+			if (c[i].expo == c[j].expo)
+			{
+				c[i].coeff += c[j].coeff;
+				c[j].coeff = 0;
+			}
+		}
+	}
+	printf(" \nResultant polynomial: ");
+	printf("%d(x^%d)", c[0].coeff, c[0].expo);
+	for (int k = 1; k < t3; k++)
+	{
+		printf("+%d(x^%d)", c[k].coeff, c[k].expo);
+	}
 	return 0;
 }
